724c670a467829ca6708bd79783cc031f6aed427
nguyentrungduc <nguyentrungduc08@gmail.com>
2018-07-03 15:30:43 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: cpu10664-local
+ *
+ * Created on July 3, 2018, 3:21 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
